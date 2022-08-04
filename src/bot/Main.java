ab74f7a25ac77b905b847197a9fef10b56db8560
12emin34 <macanovic.emin@gmail.com>
2022-08-04 15:02:32 +0200

    dodan TODO za fazu 2

--- src/bot/Main.java
+++ src/bot/Main.java
@@ -2,7 +2,7 @@ package bot;
 
 public class Main {
     public static void main(String[] args) {
-        //
         System.out.println("Pozdrav ja sam Hazimova Kafana, napravljen sam 4.8.2022 u 14:07h.");
+        // TODO Bot treba pitati korisnika kako se zove, i odgovoriti mu nešto
     }
 }
