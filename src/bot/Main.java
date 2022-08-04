015e55a7a068bed4654f126e74f38895d9073b35
ProZzZz_Gamer <amiinn.lulicami878@gmail.com>
2022-08-04 15:09:14 +0200

    2. korak u pravljenju bota gotov (ispitivanje o imenu i dobrodošlica)

--- src/bot/Main.java
+++ src/bot/Main.java
@@ -1,8 +1,17 @@
 package bot;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Pozdrav ja sam Hazimova Kafana, napravljen sam 4.8.2022 u 14:07h.");
-        // TODO Bot treba pitati korisnika kako se zove, i odgovoriti mu nešto
+
+        System.out.println("Kako se zoveš?");
+
+        Scanner unos = new Scanner(System.in);
+        String ime = unos.nextLine();
+
+        System.out.println(ime + " dobrodošao na server :D!");
+
     }
 }
