0e128f587de11f30f6170ae24f4929cd2d3906a9
ProZzZz_Gamer <amiinn.lulicami878@gmail.com>
2022-08-04 18:06:47 +0200

    3. korak gotov (računanje godina)

--- src/bot/Main.java
+++ src/bot/Main.java
@@ -11,9 +11,16 @@ public class Main {
         Scanner unos = new Scanner(System.in);
         String ime = unos.nextLine();
 
-        System.out.println(ime + " dobrodošao na server :D!");
-        // TODO napravi da bot pogodi koliko godina
-        // godine = (ostatak3 * 70 + ostatak5 * 21 + ostatak7 * 15) % 105
-        // bot treba uzeti ostatak3/5/7 iz unosa, te varijable predstavljaju ostatak od dijeljenja korisnikovih godina sa tim brojevima.
+        System.out.println(ime + " dobrodošao/la na server :D!");
+
+        System.out.println("Da bi dalje nastavio/la konverzaciju, upiši ostatak broja svojih godina sa 3 i upiši u knzolu, tako sa 5 i 7. :D");
+
+        int o3 = unos.nextInt();
+        int o5 = unos.nextInt();
+        int o7 = unos.nextInt();
+
+        System.out.print("Tvoj broj godina je ");
+        System.out.println((o3 * 70 + o5 * 21 + o7 * 15) % 105);
+
     }
 }
