99c3ad63b4cf7148667b0a0893135c880a2c6bb3
12emin34 <macanovic.emin@gmail.com>
2022-08-04 14:03:22 +0200

    dodan TODO za pozdrav bota

---
+++ src/bot/Main.java
@@ -0,0 +1,8 @@
+package bot;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO Bot bi se trebao predstaviti, tj trebao bi reći svoje ime i godinu kad je napravljen.
+        System.out.println("UAUAUA");
+    }
+}
